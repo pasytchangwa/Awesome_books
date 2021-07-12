610314620e7df0ca3b18ce9d4e783b5ee0cfcba5
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-12 15:57:26 +0300

    Called Dom elements

---
+++ app.js
@@ -0,0 +1,6 @@
+const mainSect = document.querySelector('.main-section');
+const mainTitle = document.querySelector('.main-title');
+const bookList = document.querySelector('add-book');
+const addTitle = document.querySelector('.title');
+const addAuthor = document.querySelector('author');
+const addBtn = document.querySelector('.add-btn');
