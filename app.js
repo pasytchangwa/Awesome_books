605d681739ecb5d9803afbdb5054ccfce663eb16
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-13 10:42:14 +0300

    Update index,html file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+// calling DOM elements
 const mainSect = document.querySelector('.main-section');
 const mainTitle = document.querySelector('.main-title');
-const bookList = document.querySelector('add-book');
-const addTitle = document.querySelector('.title');
-const addAuthor = document.querySelector('author');
+const addingBook = document.querySelector('book-list');
 const addBtn = document.querySelector('.add-btn');
+const addTitle = document.querySelector('.title').value;
+const addAuthor = document.querySelector('.author').value;
+
+ // Create a collection that keeps a list of books (hint: you can use an array of objects for that).
+
+ let bookList = [];
