d7c736e25d11a8208d1eff4d29cd674b62a8a551
Sylvestre <64914462+pasytchangwa@users.noreply.github.com>
2021-07-14 11:50:00 +0300

    Modify list of books for single books to be in a single container

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -11,49 +11,53 @@ class BooksCollection {
     this.collection = [];
   }
 
- DisplayBooks = (itemarr) => {
-   const bookLists = itemarr.map((item) => `
-  <li>${item.title}</li>
-  <li>${item.author}</li>
-  <button type='button' id='${item.id}' class='remove-btn'>Remove</button>
-  `).join('');
-   addingBook.innerHTML = bookLists;
- }
+  DisplayBooks = (itemarr) => {
+    const bookLists = itemarr
+      .map(
+        (item) => `
+  <li class='list-item'><p>${item.title} by ${item.author}</p>
+  <button type='button' id='${item.id}' class='remove-btn'>Remove</button></li>
+  `,
+      )
+      .join('');
+    addingBook.innerHTML = bookLists;
+  };
 
- addItem = () => {
-   j += 1;
-   const singleBook = {
-     id: j,
-     title: addTitle.value,
-     author: addAuthor.value,
-   };
-   this.collection.push(singleBook);
-   localStorage.setItem('bookObject', JSON.stringify(this.collection));
-   this.DisplayBooks(this.collection);
-   addingBook.style.listStyle = 'none';
-   addingBook.style.width = '100%';
-   addingBook.style.display = 'flex';
-   addingBook.style.flexDirection = 'column';
-   addingBook.style.alignItems = 'start';
-   addingBook.addEventListener('click', (e) => {
-     if (e.target.classList.contains('remove-btn')) {
-       this.removeItem(e);
-     }
-   });
-   form.reset();
- }
+  addItem = () => {
+    j += 1;
+    const singleBook = {
+      id: j,
+      title: addTitle.value,
+      author: addAuthor.value,
+    };
+    this.collection.push(singleBook);
+    localStorage.setItem('bookObject', JSON.stringify(this.collection));
+    this.DisplayBooks(this.collection);
+    addingBook.style.listStyle = 'none';
+    addingBook.style.width = '100%';
+    addingBook.style.display = 'flex';
+    addingBook.style.flexDirection = 'column';
+    addingBook.style.alignItems = 'start';
+    addingBook.addEventListener('click', (e) => {
+      if (e.target.classList.contains('remove-btn')) {
+        this.removeItem(e);
+      }
+    });
+    form.reset();
+  };
 
- removeItem = (ev) => {
-   const removeId = ev.target.id;
-   this.collection = this.collection.filter(
-     (x) => x
-       !== this.collection[
-         this.collection.findIndex((y) => y.id === parseInt(removeId, 10))
-       ],
-   );
-   localStorage.setItem('bookObject', JSON.stringify(this.collection));
-   this.DisplayBooks(this.collection);
- }
+  removeItem = (ev) => {
+    const removeId = ev.target.id;
+    this.collection = this.collection.filter(
+      (x) =>
+        x !==
+        this.collection[
+          this.collection.findIndex((y) => y.id === parseInt(removeId, 10))
+        ]
+    );
+    localStorage.setItem('bookObject', JSON.stringify(this.collection));
+    this.DisplayBooks(this.collection);
+  };
 }
 const collectedBooks = new BooksCollection();
 window.onload = () => {
